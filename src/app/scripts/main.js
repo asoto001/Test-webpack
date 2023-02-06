33cb64d8d480210aa38ed6f251444a6339d65bb6
alejandro <soto.millan.ceni@gmail.com>
2023-02-06 17:48:28 -0500

    try

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,3 +1,23 @@
 import '../styles/style.scss';
+import axios from 'axios';
+
+const form = document.getElementById('form');
+
+const getPokemons = async () => {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+    response.data.results.forEach(element => {
+        document.body.innerHTML += `
+        <hr>
+        ${element.name}
+        `
+    });
+}
+
+getPokemons();
+
+form.addEventListener('submit', () => {
+    e.preventDefault();
+    SVGFEDropShadowElement.fire("Good Job!" , "You clicked the button!", "success");
+})
 
 console.log('hola mundo');
